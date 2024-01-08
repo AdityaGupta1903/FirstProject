785f9f1702f5e701193d9cf3a16c6df6214ba750
Aditya Gupta <adityagu@vconstruct.in>
2024-01-08 15:17:38 +0530

    first Commit

---
+++ index.js
@@ -0,0 +1,28 @@
+document.getElementsByTagName('img')[0].addEventListener('click',()=>{
+    let audio = new Audio('sounds/crash.mp3');
+    audio.play();
+})
+document.getElementsByTagName('img')[1].addEventListener('click',()=>{
+    let audio = new Audio('sounds/kick-bass.mp3');
+    audio.play();
+})
+document.getElementsByTagName('img')[2].addEventListener('click',()=>{
+    let audio = new Audio('sounds/snare.mp3');
+    audio.play();
+})
+document.getElementsByTagName('img')[3].addEventListener('click',()=>{
+    let audio = new Audio('sounds/tom-1.mp3');
+    audio.play();
+})
+document.getElementsByTagName('img')[4].addEventListener('click',()=>{
+    let audio = new Audio('sounds/tom-2.mp3');
+    audio.play();
+})
+document.getElementsByTagName('img')[5].addEventListener('click',()=>{
+    let audio = new Audio('sounds/tom-3.mp3');
+    audio.play();
+})
+document.getElementsByTagName('img')[6].addEventListener('click',()=>{
+    let audio = new Audio('sounds/tom-4.mp3');
+    audio.play();
+})
